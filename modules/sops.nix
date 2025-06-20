038af35b65492e4536939cb4813939dafab1cb41
Ibrahim <33922797+SleepySwords@users.noreply.github.com>
2025-06-20 16:25:26 +1000

    chore: update keys

--- modules/sops.nix
+++ modules/sops.nix
@@ -4,7 +4,8 @@
     inputs.sops-nix.nixosModules.sops
   ];
   sops.age.keyFile = "/var/lib/sops-nix/key.txt";
-  sops.secrets."test" = {
-    sopsFile = ../secrets/homelab.yaml;
+  sops.defaultSopsFile = ../secrets/homelab.yaml;
+  sops.secrets.hello = {
+    format = "yaml"
   };
 }
