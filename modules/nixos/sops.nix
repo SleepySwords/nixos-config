01b48487fc523a9b27313e7bae5a311a0c838e37
Ibrahim <33922797+SleepySwords@users.noreply.github.com>
2025-11-13 18:11:24 +1100

    feat: whoops moving x made things break

--- modules/nixos/sops.nix
+++ modules/nixos/sops.nix
@@ -4,20 +4,20 @@
     inputs.sops-nix.nixosModules.sops
   ];
   sops.age.keyFile = "/var/lib/sops-nix/key.txt";
-  sops.defaultSopsFile = ../secrets/homelab.yaml;
+  sops.defaultSopsFile = ../../secrets/homelab.yaml;
   sops.secrets.hello = {
     format = "yaml";
   };
 
   sops.secrets."cloudflare.env" = {
-    sopsFile = ../secrets/cloudflare.env;
+    sopsFile = ../../secrets/cloudflare.env;
     path = "/run/secrets/cloudflare.env";
     format = "dotenv";
     key = "";
   };
 
   sops.secrets."cloudflared.json" = {
-    sopsFile = ../secrets/cloudflared.json;
+    sopsFile = ../../secrets/cloudflared.json;
     path = "/run/secrets/cloudflared.json";
     format = "json";
     key = "";
